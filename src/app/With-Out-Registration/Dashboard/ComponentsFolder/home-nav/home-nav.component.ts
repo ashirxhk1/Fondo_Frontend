9944d3eb270f1c85ab76ac234b3a51bd805f459d
anas <anas@saufik.com>
2024-06-13 03:13:11 +0500

    Uodahe sakjsdkl;askdkj;kasjlkadskadsjkas

--- src/app/With-Out-Registration/Dashboard/ComponentsFolder/home-nav/home-nav.component.ts
+++ src/app/With-Out-Registration/Dashboard/ComponentsFolder/home-nav/home-nav.component.ts
@@ -17,43 +17,43 @@ constructor(private route :Router){}
 
   TopRestaurantItems = [
     
-    {id:1,  icon:'/assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
-    {id:2,  icon:'/assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToFollower'},
-    {id:3,  icon:'/assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
-    {id:4,  icon:'/assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
-    {id:5,  icon:'/assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
-    {id:6,  icon:'/assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
-    {id:7,  icon:'/assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
-    {id:8,  icon:'/assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
+    {id:1,  icon:'./assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
+    {id:2,  icon:'./assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToFollower'},
+    {id:3,  icon:'./assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
+    {id:4,  icon:'./assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
+    {id:5,  icon:'./assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
+    {id:6,  icon:'./assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
+    {id:7,  icon:'./assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
+    {id:8,  icon:'./assets/top-restaurant.jpg', name: 'Food Restaurantt' , rank: '4.5', url:'goToDetail'},
     
   ]
 items: any;
 
 TopSellerProducts =[
-  {id:1, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
-  {id:2, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
-  {id:3, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
-  {id:4, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
-  {id:5, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
-  {id:6, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
-  {id:7, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
-  {id:8, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
-  {id:9, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
-  {id:10, icon:'/assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:1, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:2, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:3, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:4, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:5, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:6, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:7, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:8, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:9, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
+  {id:10, icon:'./assets/top-prodeuct-2.jpg', name:'Chicken Pizza', price:'$ 120.20', rating:'4.5k' },
 
 ]
 
 HumburgurItems =[
-  {id:1, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
-  {id:2, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
-  {id:3, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
-  {id:4, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
-  {id:5, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
-  {id:6, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
-  {id:7, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
-  {id:8, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
-  {id:9, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
-  {id:10, icon:'/assets/Top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:1, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:2, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:3, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:4, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:5, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:6, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:7, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:8, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:9, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
+  {id:10, icon:'./assets/top-Seller.jpg', name:'Chicken Pizza', rating:'4.5k' },
 
 ]
 
